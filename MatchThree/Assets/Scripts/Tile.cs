58d5d1bb2b14e673154bedcfa2aa533f9d09d6c6
Stanley-A <Alicedaisy.stanley@hotmail.com>
2022-05-20 10:29:35 +0100

    Obstacle Tiles

--- MatchThree/Assets/Scripts/Tile.cs
+++ MatchThree/Assets/Scripts/Tile.cs
@@ -2,14 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum TileType
+{
+    Normal,
+    Obstacle
+}
+
 public class Tile : MonoBehaviour
 {
 
     public int xIndex;
     public int yIndex;
 
+
     Board m_board;
 
+    public TileType tileType = TileType.Normal;
 
     // Start is called before the first frame update
     void Start()
